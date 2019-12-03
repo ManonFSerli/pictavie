0b697bb8a77d0be5f7adfbc30da07b9b1054f13c
Manon <manon.faure@serli.com>
2019-12-03 14:59:36 +0100

    Updates

---
+++ precache-manifest.ea6b34c2cc2aff411f97bf346b4ed7e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6849aa1dc676c95b462ac4b006cf2e39",
+    "url": "/pictavie/index.html"
+  },
+  {
+    "revision": "3f2564667601ae27b3ec",
+    "url": "/pictavie/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "55e6284d8bc061df375c",
+    "url": "/pictavie/static/js/2.913e0a00.chunk.js"
+  },
+  {
+    "revision": "3f2564667601ae27b3ec",
+    "url": "/pictavie/static/js/main.16e572d0.chunk.js"
+  },
+  {
+    "revision": "c23773010f2b7bd69dbf",
+    "url": "/pictavie/static/js/runtime-main.2f516919.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/pictavie/static/media/logo.25bf045c.svg"
+  }
+]);
